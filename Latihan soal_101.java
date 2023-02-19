21417650a6dc772214c9322ec3ec1195bb1bfdc3
VikaOkta <125806231+VikaOkta@users.noreply.github.com>
2023-02-19 20:43:06 +0700

    Add files via upload

---
+++ Latihan soal_101.java
@@ -0,0 +1,6 @@
+run:
+Program Luas Lingkaran
+
+Masukkan Panjang Jari-jari : 50
+Luas Lingkaran = 3925 
+Keliling lingkaran = 314BUILD SUCCESSFUL (total time: 9 seconds)
---
+++ LatihanJava1.java
@@ -0,0 +1,11 @@
+public class LatihanJava1 {
+
+public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Vika Okta Fitriana");
+        System.out.println("XI Tel 11");
+        System.out.println("RPL");
+        
+                
+    }
+}
